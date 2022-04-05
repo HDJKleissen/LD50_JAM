17475d472717fa0758eebdf47515103a105cc181
Kleissen,H.D.J. (Hilko) <h.d.j.kleissen@students.uu.nl>
2022-04-05 02:45:24 +0200

    Add WEBGL specific stuff

---
+++ Unity Project/LD50_JAM/Assets/Scripts/WebGLMusicChecker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WebGLMusicChecker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (FMODUnity.RuntimeManager.HasBankLoaded("Master")
+            && FMODUnity.RuntimeManager.HasBankLoaded("Master.strings")
+            )
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+        else
+        {
+            StartCoroutine(CoroutineHelper.Chain(
+                CoroutineHelper.WaitUntil(() => FMODUnity.RuntimeManager.HasBankLoaded("Master")),
+                CoroutineHelper.WaitUntil(() => FMODUnity.RuntimeManager.HasBankLoaded("Master.strings")),
+                CoroutineHelper.Do(() => SceneManager.LoadScene("MainMenu"))
+            ));
+        }
+    }
+}
